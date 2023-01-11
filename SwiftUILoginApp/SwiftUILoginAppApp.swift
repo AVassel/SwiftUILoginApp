eb2d34533a5213bdf9c64f4051579052db1f0886
A.Vassel <102523970+AVassel@users.noreply.github.com>
2023-01-11 15:24:40 +0300

    Initial Commit

---
+++ SwiftUILoginApp/SwiftUILoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILoginAppApp.swift
+//  SwiftUILoginApp
+//
+//  Created by Anton Vassel on 11.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
